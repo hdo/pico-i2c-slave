f59481463168ad5f3819cff45e4037243bed64a8
Huy Do <huydo1@gmail.com>
2021-05-05 16:16:18 +0200

    i2c slave with interrupts

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <math.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/irq.h"
 
 
 #define I2C_PORT i2c0
@@ -15,11 +16,36 @@
 #define MODEID_CHALLENGE 4
 #define MODEID_SERIAL    5
 
+
+// define the hardware registers used
+volatile uint32_t * const I2C0_DATA_CMD       = (volatile uint32_t * const)(I2C0_BASE + 0x10);
+volatile uint32_t * const I2C0_INTR_STAT      = (volatile uint32_t * const)(I2C0_BASE + 0x2c);
+volatile uint32_t * const I2C0_INTR_MASK      = (volatile uint32_t * const)(I2C0_BASE + 0x30);
+volatile uint32_t * const I2C0_CLR_RD_REQ     = (volatile uint32_t * const)(I2C0_BASE + 0x50);
+volatile uint32_t * const I2C0_CLR_STOP_DET   = (volatile uint32_t * const)(I2C0_BASE + 0x60);
+volatile uint32_t * const I2C0_CLR_START_DET  = (volatile uint32_t * const)(I2C0_BASE + 0x64);
+
+// Declare the bits in the registers we use
+#define I2C_DATA_CMD_FIRST_BYTE  0x00000800
+#define I2C_DATA_CMD_DATA        0x000000ff
+#define I2C_INTR_STAT_READ_REQ   0x00000020
+#define I2C_INTR_STAT_RX_FULL    0x00000004
+#define I2C_INTR_STAT_START_DET  (1 << 10)   // bit 10
+#define I2C_INTR_MASK_READ_REQ   0x00000020  // bit 5 (1 << 5)
+#define I2C_INTR_MASK_RX_FULL    0x00000004
+#define I2C_INTR_MASK_START_DET  (1 << 10)   // bit 10
+
+#define I2C_INTR_STAT_STOP_DET  (1 << 9)   
+#define I2C_INTR_MAKS_STOP_DET  (1 << 9)   
+
+
 const uint8_t LED_PIN = 25;
 const uint8_t IDENT_STRING[] = "Handrad 0001";
 const uint8_t SERIAL_STRING[] = "ABCD12345678";
 const uint8_t CHALLENGE_VALUE[] = {0x3B, 0x59, 0xE8, 0x2A, 0xE9, 0xB1, 0xBE, 0xD8, 0x00, 0x00, 0x00, 0x00};
 
+uint8_t rx_head = 0;
+uint8_t tx_head = 0;
 uint8_t rxdata[16];
 uint8_t txdata[16];
 uint8_t frame_num = 0;
@@ -29,7 +55,71 @@ uint32_t current_tick()  {
 }
 
 
-void send_reply() {
+void printhex(uint8_t *buffer, uint8_t length) {
+    for(uint8_t i = 0; i < length; i++) {
+        printf("%02X ", buffer[i]);
+    }
+    printf("\r\n");
+}
+
+void i2c0_irq_handler() {
+    // Get interrupt status
+    uint32_t status = *I2C0_INTR_STAT;
+
+    // check for start condition
+    if (status & I2C_INTR_STAT_START_DET) {
+        // reset heads
+        tx_head = 0;
+        rx_head = 0;
+        *I2C0_CLR_START_DET;
+    }
+
+    // check for stop condition
+    if (status & I2C_INTR_STAT_STOP_DET) {
+        // reset heads
+        tx_head = 0;
+        rx_head = 0;
+        *I2C0_CLR_STOP_DET;
+    }
+
+    // Check to see if we have received data from the I2C master
+    if (status & I2C_INTR_STAT_RX_FULL) {
+        // Read the data (this will clear the interrupt)
+        uint32_t value = *I2C0_DATA_CMD;
+        // Check if this is the 1st byte we have received
+        if (value & I2C_DATA_CMD_FIRST_BYTE) {
+            // 
+            rx_head = 0;
+            // If so treat it as the address to use
+          
+        } 
+        rxdata[rx_head++] = (uint8_t)(value & I2C_DATA_CMD_DATA);
+
+        if (rx_head == READ_SIZE) {
+            printhex(rxdata, READ_SIZE);
+        }
+
+        if (rx_head > READ_SIZE) {
+            rx_head = 0;
+            puts("rx out of bounds");
+        }
+    }
+
+    // Check to see if the I2C master is requesting data from us
+    if (status & I2C_INTR_STAT_READ_REQ) {
+        // Write the data from the current address in RAM
+        *I2C0_DATA_CMD = (uint32_t) txdata[tx_head++];
+        // Clear the interrupt
+        *I2C0_CLR_RD_REQ;
+        if (tx_head > WRITE_SIZE) {
+            tx_head = 0;
+            puts("tx out of bounds");
+        }
+    }
+}
+
+
+void prepare_reply() {
     uint8_t checksum = 0;
     // increase frame number
     txdata[13] = frame_num++;
@@ -37,9 +127,7 @@ void send_reply() {
         checksum ^= txdata[i];
         checksum++;
     }
-    txdata[14] = checksum;
-    
-    i2c_write_raw_blocking(I2C_PORT, txdata, WRITE_SIZE);
+    txdata[14] = checksum;    
 }
 
 
@@ -51,7 +139,7 @@ void mode_normal() {
     txdata[0] = 0xFF;
     txdata[1] = 0b11111111;
     txdata[12] = MODEID_NORMAL;
-    send_reply();
+    prepare_reply();
 }
 
 void mode_identify() {
@@ -59,7 +147,7 @@ void mode_identify() {
         txdata[i] = IDENT_STRING[i];
     }
     txdata[12] = MODEID_IDENTIFY;
-    send_reply();
+    prepare_reply();
 }
 
 void mode_challenge() {
@@ -67,7 +155,7 @@ void mode_challenge() {
         txdata[i] = CHALLENGE_VALUE[i];
     }    
     txdata[12] = MODEID_CHALLENGE;
-    send_reply();
+    prepare_reply();
 }
 
 void mode_serial() {
@@ -75,7 +163,14 @@ void mode_serial() {
         txdata[i] = SERIAL_STRING[i];
     }
     txdata[12] = MODEID_SERIAL;
-    send_reply();
+    prepare_reply();
+}
+
+
+void setup_mock() {
+    for(uint8_t i=0; i < sizeof(txdata); i++) {
+        txdata[i] = i;
+    }
 }
 
 
@@ -83,6 +178,7 @@ int main() {
     stdio_init_all();
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
 
 
     // This example will use I2C0 on the default SDA and SCL pins (4, 5 on a Pico)
@@ -94,26 +190,32 @@ int main() {
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
-    for(int i=0; i < 5; i++) {
+    setup_mock();
+
+
+    // blinky
+    for(int i=0; i < 3; i++) {
         gpio_put(LED_PIN, 0);
         sleep_ms(150);
         gpio_put(LED_PIN, 1);
     }
 
     puts("Started ...\n");
+    printhex(txdata, 15);
+
+
+    // Enable the interrupts we want
+    *I2C0_INTR_MASK = (I2C_INTR_MASK_READ_REQ | I2C_INTR_MASK_RX_FULL | I2C_INTR_MASK_START_DET);
+    //*I2C0_INTR_MASK = (I2C_INTR_MASK_READ_REQ | I2C_INTR_MASK_RX_FULL);
+
+    // Set up the interrupt handlers
+    irq_set_exclusive_handler(I2C0_IRQ, i2c0_irq_handler);
+    // Enable I2C interrupts
+    irq_set_enabled(I2C0_IRQ, true);
+
 
     while (1) {
-        if (i2c_get_read_available(I2C_PORT) >= READ_SIZE) {
-            i2c_read_raw_blocking (I2C_PORT, rxdata, READ_SIZE);
-            
-            uint8_t mode = rxdata[0];
-            switch(mode) {
-                case MODEID_IDENTIFY: mode_identify(); break;
-                case MODEID_CHALLENGE: mode_challenge(); break;
-                case MODEID_SERIAL: mode_serial(); break;
-                default: mode_normal(); break;
-            }            
-        }            
-        sleep_ms(10);                   
+        puts("ping");
+        sleep_ms(500);                   
     }
 }
